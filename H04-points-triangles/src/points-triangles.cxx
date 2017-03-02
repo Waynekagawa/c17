40285c97f54196908087a276b571b51dabdff4fa
Waynekagawa <karandtaneja@gmail.com>
2017-03-02 17:43:27 +0100

    Update points-triangles.cxx

--- H04-points-triangles/src/points-triangles.cxx
+++ H04-points-triangles/src/points-triangles.cxx
@@ -11,20 +11,32 @@
 class Point
 {
 public:
-   Point(); //the default constructor
-   Point(double a, double b);{x=a;y=b} //Declaring a constructor of doubvles and initialising it to take the values of x and y
-    //not sure if this should be written here or outside in the main function. like Point.x = a or something. 
-   Point operator+(const Point& Other) const{
-       double cx = 0; double cy = 0;
-       cx += x + other.x;
-       cy += y + other.y;
-       return Point(cx,cy);// operators
+   Point(){x = 0; y= 0;}//the default constructor
+   Point(double a, double b){x=a;y=b;}//Declaring a constructor of doubles and initialising it to take the values of x and y
+    // operators
+   Point operator+(const Point& other) const{
+      Point c;//defining a third point which is the result of the addition of the other two.
+       c.x += this->x + other.x;//runs without this-> as well. Need to know why.
+       c.y += this->y + other.y;
+       return c;}
+       
+ Point operator-(const Point& other) const{
+      Point c;//defining a third point which is the result of the addition of the other two.
+       c.x += this->x - other.x;//runs without this-> as well. Need to know why.
+       c.y += this->y - other.y;
+       return c;} 
+Point operator+=(const Point& other) {
+       Point c;//defining a third point which is the result of the addition of the other two.
+       this->x = this->x + other.x;//runs without this-> as well. Need to know why.
+       this->y = this->y + other.y;
+       return c;} 
+	   //Instead of defining c, just start with replacing this->x and this->y because I have removed the const in there to facilitate the change in variable b. Return this* instead as this is a pointer.
     // methods
-
-    double x;
-    double y;
+    
+ double x;double y;
+    
 };
-
+/**
 class Triangle
 {
 public:
@@ -34,6 +46,7 @@ public:
 
     Point a, b, c;
 };
+*/
 
 // ostream operator for `Point`s
 std::ostream &operator<<(std::ostream &os, const Point &p)
@@ -52,14 +65,13 @@ std::ostream &operator<<(std::ostream &os, const Point &p)
     os.precision(current_precision);
     return os;
 }
-
+/**
 // ostream operator for `Triangle`s
 std::ostream &operator<<(std::ostream &os, const Triangle &t)
 {
     return os << "Triangle< " << t.a << ", " << t.b << ", " << t.c <<  " >";
 }
-
-
+*/
 #include <iostream>
 using namespace std;
 
@@ -68,12 +80,19 @@ int main()
     // example, replace with your tests
 
     Point p;
-    Triangle t;
+    Point a = {1, 2};
+    Point b = {3, 4};
+    Point c;// = {3, 4};
+    Point d;
+    b += a;    // result: b.x = 4, b.y = 6
+    //c = b + a; // result: c.x = 4, c.y = 6
+    //d = b - a;
+    //Triangle t;
 
     // set point `p` to (1, 2)
-    p.x = 1;
+    p.x = 211;
     p.y = 2;
-
+/**
     // set triangle `t` to (1, 2), (3, 4), (5, 6)
     t.a.x = 1;
     t.a.y = 2;
@@ -81,8 +100,11 @@ int main()
     t.b.y = 4;
     t.c.x = 5;
     t.c.y = 6;
-
+*/
     // print point `p` and triangle `t`
-    cout << p << endl;
-    cout << t << endl;
+    std::cout<<p<<std::endl;
+    //std::cout<<c<<std::endl;
+    std::cout<<b<<std::endl;
+ //   cout << t << endl;
 }
+
