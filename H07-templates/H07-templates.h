02771c7cffd219d7b1941d1b012522e4b09f8044
Waynekagawa <karandtaneja@gmail.com>
2017-03-17 12:18:43 +0100

    Update H07-templates.h

--- H07-templates/H07-templates.h
+++ H07-templates/H07-templates.h
@@ -18,16 +18,32 @@ auto add(T1 a, T2 b)->decltype(a+b)
 
 template<typename T1>
 bool is_int(T1 a)
-{
-	if (typeid(a)==int){
-	return true;}
-	else
-	{return false;}
+{ int b = 1;
+return (typeid(a).name()==typeid(b).name()); //We can directly write this instead of and if-else condition.
 }
+
+template<typename T>
+class Number{
+public:
+T value;//attribute.
+
+//constructor and destructor.
+Number():value(){}//default
+Number(T a):value(a){}
+//~Number():value(0){}
+
+//Operators
+template<typename U>
+Number<T> &operator+(const Number<U> &other){
+  value+=other.value;
+return *this;}
+};
 int main()
 {
     PRINT_EXPRESSION(add_simple(1, 2));
     PRINT_EXPRESSION(add_simple(1.5, 2.2));
     PRINT_EXPRESSION(add(1, 2));
+    PRINT_EXPRESSION(is_int("Goookaaa"));
+	PRINT_EXPRESSION();
     return 0;
 }
